f95e37e39f252c1fa730075ad6800cc1b5abcc21
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-22 22:29:52 -0400

    video plays now

--- src/Video.js
+++ src/Video.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import "./Video.css"
 
 function Video() {
+    const [playing, setPlaying] = useState(false)
+    const videoRef = useRef(null)
+    
+    const handleVideoPress = () => {
+        if (playing) {
+            videoRef.current.pause()
+            setPlaying(false)
+        } else {
+            videoRef.current.play()
+            setPlaying(true)
+        }
+    }
+
     return (
         <div className="video">
-            <video src="https://v21-us.tiktokcdn.com/video/tos/useast2a/tos-useast2a-pve-0068/00f6414927bd463b99480b6930ae3b2e/?VExpiration=1616486831&VSignature=4d1baebbce78297b014dcf50eced1e04&a=1233&br=4342&bt=2171&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202103230206590101890731653C88634D&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anl5aHF3cWk8MzMzaTczM0ApNTtpaTtpOWU8N2c3PDs3OGdpZmluX2ZkZHBgLS0uMTZzc2AxNV8vXmJgNmM1Xi5fNjY6Yw%3D%3D&vl=&vr="></video>
+            <video 
+                onClick={handleVideoPress}
+                className="video__player"
+                loop
+                ref={videoRef}
+                src="https://v21-us.tiktokcdn.com/video/tos/useast2a/tos-useast2a-pve-0068/00f6414927bd463b99480b6930ae3b2e/?VExpiration=1616486831&VSignature=4d1baebbce78297b014dcf50eced1e04&a=1233&br=4342&bt=2171&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202103230206590101890731653C88634D&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anl5aHF3cWk8MzMzaTczM0ApNTtpaTtpOWU8N2c3PDs3OGdpZmluX2ZkZHBgLS0uMTZzc2AxNV8vXmJgNmM1Xi5fNjY6Yw%3D%3D&vl=&vr="></video>
             {/* VideoFooter */}
             {/* VideoSidebarfv */}
 
