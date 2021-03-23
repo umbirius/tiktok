05ae5ac574b2d4b3fa5491fc2925366e940f6f3a
Umberto Palazzo <umbertopalazzo@MBP16.fios-router.home>
2021-03-22 22:12:20 -0400

    added video file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react'
 import './App.css';
+import Video from './Video';
 
 function App() {
   return (
+
+    // Bem naming convention
     <div className="app">
       <h1>Mern</h1>
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
+
+      { /* app container */}
+      {/* videos */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+
     </div>
   );
 }
